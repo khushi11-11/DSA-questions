0d747e105accd113f007b27269bf342d81f3bd80
Khushi Verma <khushi7991012@gmail.com>
2025-03-16 14:51:19 +0530

    problem stmt added

--- src/com/practice/fxn/Prg13_sum.java
+++ src/com/practice/fxn/Prg13_sum.java
@@ -2,6 +2,8 @@ package com.practice.fxn;
 
 import java.util.Scanner;
 
+// Write a function that returns the sum of first n natural numbers.
+
 public class Prg13_sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
