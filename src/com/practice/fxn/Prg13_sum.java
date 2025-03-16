9d92ddd6a4d330211a672defb750d518f858954f
Khushi Verma <khushi7991012@gmail.com>
2025-03-16 14:59:07 +0530

    problem stmt and code added

--- src/com/practice/fxn/Prg13_sum.java
+++ src/com/practice/fxn/Prg13_sum.java
@@ -7,5 +7,20 @@ import java.util.Scanner;
 public class Prg13_sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a natural number:- ");
+        int n = sc.nextInt();
+        System.out.println("Sum of first " + n + " natural numbers is " + sum(n));
+    }
+
+    public static int sum (int num) {
+        int sum = 0;
+         if (num >= 1){
+             for ( int i=1; i<=num; i++){
+                 sum+=i;
+             }
+         } else {
+             System.out.println("Entered number is not a natural number.");
+         }
+         return sum;
     }
 }
