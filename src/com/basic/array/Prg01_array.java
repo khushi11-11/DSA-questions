64dd6bfdc54d7cab651b41f18a0239847a77587c
Khushi Verma <khushi7991012@gmail.com>
2025-03-16 22:12:56 +0530

    basics and memory allocation in array

---
+++ src/com/basic/array/Prg01_array.java
@@ -0,0 +1,12 @@
+package com.basic.array;
+
+public class Prg01_array {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0] = 23;
+        arr[1] = 90;
+        arr[2] = 34;
+
+        System.out.println(arr[2]);
+    }
+}
