7c62834637c309e2aab035ee1512ef218b45bb84
Khushi Verma <khushi7991012@gmail.com>
2025-03-18 22:28:04 +0530

    null and 0 - commit

--- src/com/basic/array/Prg01_array.java
+++ src/com/basic/array/Prg01_array.java
@@ -3,10 +3,17 @@ package com.basic.array;
 public class Prg01_array {
     public static void main(String[] args) {
         int[] arr = new int[3];
-        arr[0] = 23;
-        arr[1] = 90;
-        arr[2] = 34;
+        arr[0] = 23; // first element
+        arr[1] = 90; // second element
+//        arr[2] = 34; // third element
 
-        System.out.println(arr[2]);
+        System.out.println(arr[2]); // returns 0
+
+        String[] fruits = new String[5];
+        System.out.println(fruits[3]);
+//        will return 'null' on output screen, null basically means it has no value
+//        here, fruits[3] is an unset reference variable.
+//        0 is a value but null does not have a value
     }
 }
+
