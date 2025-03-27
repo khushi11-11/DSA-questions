b09a83a14ba04be1366183676ed79772cd4600cf
Khushi Verma <khushi7991012@gmail.com>
2025-03-27 23:44:09 +0530

    2 leetcode (array) questions added

---
+++ src/com/practice/array/Prg04_RichestCustomerWealth.java
@@ -0,0 +1,47 @@
+package com.practice.array;
+
+/*
+4. Richest Customer Wealth
+(https://leetcode.com/problems/richest-customer-wealth/)
+
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6
+ */
+
+
+import java.util.Arrays;
+
+public class Prg04_RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = { {1,2,3}, {3,4,5}, {12,6,1} };
+        for ( int[] i : accounts)
+            System.out.println(Arrays.toString(i));
+
+        MaximumWealth(accounts);
+    }
+
+    static void MaximumWealth(int[][] nums) {
+        int[] sum = new int[nums.length];
+//        array generated with elements as sum of all the a/c (elements) of array passes (nums)
+        for (int ac = 0; ac < nums.length; ac++) {
+            for (int i = 0; i < nums[ac].length; i++) {
+                sum[ac] += nums[ac][i];
+            }
+        }
+
+        int max = 0, index=0;
+//        comparison of all the elements of sum[] array to find the richest a/c
+        for (int k = 0; k < sum.length; k++) {
+            if (max < sum[k]) {
+                max = sum[k];
+                index = k+1;
+            }
+        }
+        System.out.println("Richest customer account no.: " + index);
+        System.out.println("Wealth: " + max);
+    }
+}
---
+++ src/com/practice/array/Prg05_ArrayShuffle.java
@@ -0,0 +1,42 @@
+package com.practice.array;
+
+/*
+5. Shuffle the Array
+(https://leetcode.com/problems/shuffle-the-array/)
+Q. Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7]
+ */
+
+
+import java.util.Arrays;
+
+public class Prg05_ArrayShuffle {
+    public static void main(String[] args) {
+        int[] arr = { 2, 5, 1, 3, 4, 7 };
+        System.out.println(Arrays.toString(arr));
+        Shuffle(arr);
+    }
+
+    static void Shuffle(int[] nums) {
+        int mid = (nums.length/2); // 6/2 = 3
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i%2==0) {
+                res[i] = nums[i/2]; // res[0,2,4,6,..] = nums[0,1,2,3,...,mid-1]
+                // res[0] = nums[0/2=0]
+                // res[2] = nums[2/2=1]
+                // res[4] = nums[4/2=2]
+            } else {
+                res[i] = nums[mid];// res[1,3,5,...] = nums[mid, mid+1, ...]
+                mid++;
+                // res[1] = nums[3]
+                // res[3] = nums[4]
+                // res[5] = nums[5]
+            }
+        }
+        System.out.println(Arrays.toString(res));
+    }
+}
