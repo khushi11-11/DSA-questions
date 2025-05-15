21542802a1c8a7f4a4ba47c3cbdd6166e5d0ab37
Khushi Verma <khushi7991012@gmail.com>
2025-05-15 14:18:33 +0530

    solution to array problem 09 added

--- src/com/practice/array/Prg09_TargetArrayInOrder.java
+++ src/com/practice/array/Prg09_TargetArrayInOrder.java
@@ -17,11 +17,12 @@ Explanation:
 nums       index     target
 0            0        [0]
 1            1        [0,1]
-2            2        [0,1,2]
+2            2        [0,1,2,0,0]
 3            2        [0,1,3,2]
 4            1        [0,4,1,3,2]
  */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Prg09_TargetArrayInOrder {
@@ -32,29 +33,23 @@ public class Prg09_TargetArrayInOrder {
     }
 
     static void result(int[] nums, int[] index) {
-        int[] target = new int[nums.length];
-        for (int i = 0; i < index.length; i++) { // i= 0 to 4
-            if ( target[index[i]] == 0 ) { // if index is
-                target[index[i]] = nums[i];
-                // target[index[0]] = nums[0] -> target[0] = 0; by table
-
-            } else {
-                // target[0,1,2,3,4] != null
-                int prev = nums[i];
-
-                // we can have 2 references - current, previous
-                // curr -> last-1 (bcoz there's always a place empty at the end when one element is still remaining to be put in the array) index (w/ decr) and prev -> previous to curr
-                // put curr =to=> temp, prev=to=> curr, temp=to=> curr+1
-
-                // curr (req index) ko daalo temp me
-                // curr ko fill kro with nums[i]
-                    // now comes a loop
-                    // temp me stored h curr ki val
-                    // next will point to index+1 of target
-                    //
-
-            }
+        ArrayList<Integer> target = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) { // at i = 3, target={0,1,2,0,0}
+            target.add(index[i], nums[i]);
         }
-        System.out.println(Arrays.toString(target));
+        System.out.println(target);
+
+//        int[] arr = new int[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            if(arr[index[i]] > 0) {
+//                for (int j = nums.length; j < index[i]; j++) {
+//                    arr[index[j]] = arr[index[j-1]];
+//                }
+//                arr[index[i]] = nums[i];
+//            } else {
+//                arr[index[i]] = nums[i];
+//            }
+//        }
+//        System.out.println(Arrays.toString(arr));
     }
 }
